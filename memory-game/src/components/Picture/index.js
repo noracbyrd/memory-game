36577a189cc1b21c18973cd110b1c746164c561d
noracyrd <norabyrd@alumni.upenn.edu>
2019-11-04 00:09:03 -0600

    logic works, game works, soooorrrt of in a table, just need the shuffle stuff to work...

--- memory-game/src/components/Picture/index.js
+++ memory-game/src/components/Picture/index.js
@@ -3,9 +3,7 @@ import "./style.css";
 
 function Picture (props) {
     return (
-        <div>
-            {props.pictures.map(picture => (<span className={props[picture.id]} {...props} onClick={props.onClick} data-value={picture.id}><li><img id={picture.id.toString()} alt={picture.name}src={picture.image}></img></li></span>))}
-        </div>
+        <td>{props.pictures.map(picture => (<span className={props[picture.id]} {...props} onClick={props.onClick} data-value={picture.id}><img id={picture.id.toString()} alt={picture.name}src={picture.image}></img></span>))}</td>
     )
 }
 
--- memory-game/src/components/PictureContainer/index.js
+++ memory-game/src/components/PictureContainer/index.js
@@ -1,14 +1,18 @@
-// Children: Picture
+// Children: Row
 import React from "react";
 import "./style.css";
 import Picture from "../Picture"
 
 function PictureContainer (props) {
     return (
-        <div>
-            <ul>
-                <Picture onClick={props.handlePictureClick} pictures={props.pictures} pictureid={props} clicked={props.clicked}/>
-            </ul>
+        <div className="container">
+            <table>
+                <tbody>
+                    <tr>
+                   <Picture onClick={props.handlePictureClick} pictures={props.pictures} pictureid={props} clicked={props.clicked}/>
+                  </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
